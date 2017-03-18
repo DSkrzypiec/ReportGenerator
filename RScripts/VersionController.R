5b5e8a7529a18fc226e155be3058d5d6678bddf2
DSkrzypiec <ds320335@students.mimuw.edu.pl>
2017-03-18 22:39:44 +0100

    Add versioning reports results by Country and Date of competitions.

--- RScripts/VersionController.R
+++ RScripts/VersionController.R
@@ -96,6 +96,80 @@ create_VersionController <- function()
         saveRDS(object = newLogger, file = "./Results/Log/Log.rds")
     }
 
+    
+    # The following method create catalog for particular report
+    versionController$CreateReportCatalog <- function(ReportDate, ReportCountry)
+    {
+        if (!is.character(ReportDate))
+            stop("Given argument <ReportDate> has invalid type. Character expected.")
+        
+        if (!is.character(ReportCountry))
+            stop("Given argument <ReportCountry> has invalid type. Character expected.")
+        
+        # Check for Results catalog
+        if (!"Results" %in% dir())
+            versionController$SetUpEnv()
+        
+        # Check if given country exists in Results
+        if (!ReportCountry %in% dir("./Results/Reports"))
+            dir.create(paste0("./Results/Reports/", ReportCountry))
+        
+        if (!ReportCountry %in% dir("./Results/RObjects/"))
+            dir.create(paste0("./Results/RObjects/", ReportCountry))
+        
+        compCountryPaths <- list(reports  = paste0("./Results/Reports/", ReportCountry),
+                                 rObjects = paste0("./Results/RObjects/", ReportCountry)
+                              )
+        
+        # Check if given date exists in compCountryPaths
+        if (!ReportDate %in% dir(compCountryPaths$reports))
+            dir.create(paste0(compCountryPaths$reports, "/", ReportDate))
+        
+        if (!ReportDate %in% dir(compCountryPaths$rObjects))
+            dir.create(paste0(compCountryPaths$rObjects, "/", ReportDate))
+        
+        # Read number or report
+        resultsLog <- versionController$ReadLog()
+        resultsLogFiltered <- resultsLog$ReportId[resultsLog$ReportDate == ReportDate & 
+                                                 resultsLog$ReportCountry == ReportCountry]
+        
+        if (length(resultsLogFiltered) == 0)
+            calculationId <- 0
+        else
+            calculationId <- max(resultsLogFiltered)
+        
+        
+        compCountryDatePaths <- list(reports  = paste0("./Results/Reports/", ReportCountry, "/", ReportDate),
+                                     rObjects = paste0("./Results/RObjects/", ReportCountry, "/", ReportDate)
+                                    )
+        
+        calculationTitle <- paste0("Report_", ifelse(calculationId < 10, "0", ""), calculationId)
+        
+        # Check if Report_xx exists
+        if (!calculationTitle %in% dir(compCountryDatePaths$reports))
+            dir.create(paste0(compCountryDatePaths$reports, "/", calculationTitle))
+        
+        if (!calculationTitle %in% dir(compCountryDatePaths$rObjects))
+            dir.create(paste0(compCountryDatePaths$rObjects, "/", calculationTitle))  
+    }
+    
+    
+    # The following method create path for report
+    versionController$GetPaths <- function(ReportDate, ReportCountry, ReportId)
+    {
+        return(list(reportPath = paste0("./Results/Reports/", 
+                                        ReportCountry, "/", 
+                                        ReportDate, "/Report_", 
+                                        ifelse(ReportId < 10, "0", ""), ReportId),
+                    rObjectsPath = paste0("./Results/RObjects/", 
+                                          ReportCountry, "/", 
+                                          ReportDate, "/Report_",
+                                          ifelse(ReportId < 10, "0", ""), ReportId)
+                )
+        )
+    }
+    
+    
     return(versionController)
 }
 
