1e0656e1a7b89c44d08b8c0f8b299a9e7a3e7233
DSkrzypiec <ds320335@students.mimuw.edu.pl>
2017-03-18 02:05:33 +0100

    Add main method GenerateReport() without archivization yet

--- RScripts/ReportDesigner.R
+++ RScripts/ReportDesigner.R
@@ -11,34 +11,92 @@
 
 
 
-create_reportDesigner <- function(dataPrep)
+create_reportDesigner <- function(dataPrep, reportDate, reportCountry)
 {
+    # ----------------------------------------------------
+    # Arguments validation
+    # ----------------------------------------------------
+    
     if (class(dataPrep) != "dataPreparation")
         stop("Invalid argument <dataPrep>. Excpected object of class 'dataPreparation'.")
     
+    if (!is.character(reportDate))
+        stop("Given argument <reportDate> has incorrect type. Character was excepcted.")
     
-    # Create a new object
+    if (!reportDate %in% dataPrep$CompetitionDates)
+        stop(paste0("Given argument reportDate (", reportDate, ") is invalid. There is not such a date in data."))
+    
+    if (!is.character(reportCountry))
+        stop("Given argument <reportCountry> has incorrect type. Character was excepcted.")
+    
+    if (!reportCountry %in% dataPrep$CompetitionCountries)
+        stop(paste0("Given argument reportDate (", reportCountry, ") is invalid. There is not such a date in data."))
+    
+    
+    # ----------------------------------------------------
+    # Create empty <reportDesigner> object
+    # ----------------------------------------------------
+    
     reportDesigner        <- new.env()
     class(reportDesigner) <- "reportDesigner"
     
     
-    # Main private method for design report
-    .designReport <- function(reportDate, reportCountry)
+    
+    # ----------------------------------------------------
+    # Private properties
+    # ----------------------------------------------------
+    
+    competitionInfo <- dataPrep$FilterCompetitions(competitionDate = reportDate,
+                                                   competitionCountry = reportCountry)
+    
+    competitions    <- competitionInfo %>% dataPrep$ToCompetitionList()
+    
+    reportsTitle    <- paste0("Rubik's Cube Finalas for competitions in ", reportCountry, " on ", reportDate, ".")
+    
+    # ----------------------------------------------------
+    # Private methods
+    # ----------------------------------------------------
+    
+    .getResult <- function(competitionResults)
     {
-        if (!is.character(reportDate))
-            stop("Invalid argument type for <reportDate>. Character excpected.")
-        
-        if (!is.character(reportCountry))
-            stop("Invalid argument type for <reportCountry>. Character excpected.")
-        
-       
-        
-        
-        # TODO[1] = Histogram of results1, results2, ... 
-        # TODO[2] = Foreach competition table of resuls
-        
+        if (!is.data.frame(competitionResults))
+            stop("Invalid argument <competitionResults>. Data.frame expected.")
+            
+        return(competitionResults %>%
+                        dplyr::select(Competitor, Position, Result1, Result2, Result3, Result4, Result5) %>%
+                        dplyr::arrange(Position) %>%
+                        as.data.frame()
+        )
     }
     
+    .getCompetitionTitle <- function(competitionResults)
+    {
+        if (!is.data.frame(competitionResults))
+            stop("Invalid argument <competitionResults>. Data.frame expected.")
+        
+        return(unique(competitionResults$Competition)[1])
+    }
+    
+    
+    # ----------------------------------------------------
+    # Main public method generating report
+    # ----------------------------------------------------
+    
+    reportDesigner$GenerateReport <- function()
+    {
+        # Create empty report
+        report = ReporteRs::docx(title = "Report")
+        report = ReporteRs::addTitle(doc = report, reportsTitle, level = 1)
+        
+        # Loop over competitions
+        for (competiton in competitions)
+        {
+            report = ReporteRs::addTitle(doc = report, .getCompetitionTitle(competiton), level = 2)
+            report = ReporteRs::addFlexTable(doc = report, ReporteRs::FlexTable(.getResult(competiton)))
+        }
+        
+        ReporteRs::writeDoc(doc = report, file = "test.docx")
+    }
     
     return(reportDesigner)
 }
