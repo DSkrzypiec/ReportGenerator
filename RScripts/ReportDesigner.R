0c6f6d8ea6dedcf1ebabe09812e0470644f3dcc3
DSkrzypiec <ds320335@students.mimuw.edu.pl>
2017-03-19 19:39:55 +0100

    Add boxplot to reports and writing off R objects

--- RScripts/ReportDesigner.R
+++ RScripts/ReportDesigner.R
@@ -77,12 +77,38 @@ create_reportDesigner <- function(dataPrep, reportDate, reportCountry)
         return(unique(competitionResults$Competition)[1])
     }
     
+    .plotBoxplot <- function(competition)
+    {
+        title <- paste0("Boxplot of results for ", unique(competition$Competition)[1])
+        nobs <- nrow(competition)
+        enumerate <- c(rep(1, nobs), rep(2, nobs), rep(3, nobs), rep(4, nobs), rep(5, nobs))
+        
+        dataTransformed <- data.frame(
+                                    Label = paste0("Result", enumerate),
+                                    Result = c(competition$Result1,
+                                                competition$Result2,
+                                                competition$Result3,
+                                                competition$Result4,
+                                                competition$Result5)
+                                )
+        
+        # Remove NAs
+        dataTransformed <- dataTransformed[!is.na(dataTransformed$Result), ]
+        
+        g <- ggplot2::ggplot(data = dataTransformed, aes(x = factor(Label), y = Result)) +
+                ggplot2::geom_boxplot(fill = '#DC6900', col = 'black', alpha = 0.7) +
+                ggplot2::xlab("Rounds") + 
+                ggplot2::ylab("Solve time [in seconds]") + 
+                ggplot2::ggtitle(title)
+        
+        return(g)
+    }
     
     # ----------------------------------------------------
     # Main public method generating report
     # ----------------------------------------------------
     
-    reportDesigner$GenerateReport <- function(path)
+    reportDesigner$GenerateReport <- function(path, pathForObjects)
     {
         # Create empty report
         report = ReporteRs::docx(title = "Report")
@@ -91,8 +117,18 @@ create_reportDesigner <- function(dataPrep, reportDate, reportCountry)
         # Loop over competitions
         for (competiton in competitions)
         {
-            report = ReporteRs::addTitle(doc = report, .getCompetitionTitle(competiton), level = 2)
+            compTitle   <- unique(competiton$Competition)[1]
+            results     <- .getCompetitionTitle(competiton)
+            plot        <- suppressWarnings(.plotBoxplot(competiton))
+            
+            report = ReporteRs::addTitle(doc = report, results, level = 2)
+            report = ReporteRs::addPlot(doc = report, fun = print, x = plot)
             report = ReporteRs::addFlexTable(doc = report, ReporteRs::FlexTable(.getResult(competiton)))
+            report = ReporteRs::addPageBreak(doc = report)
+            
+            # Write RObjects
+            saveRDS(object = results, file = paste0(pathForObjects, "/", compTitle, "_results.rds"))
+            saveRDS(object = plot, file = paste0(pathForObjects, "/", compTitle, "_plot.rds"))
         }
         
         ReporteRs::writeDoc(doc = report, file = path)
